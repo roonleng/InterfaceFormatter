6f016af15ab80d7d5022d26ed4f790d108112b5f
roonleng <42977073+roonleng@users.noreply.github.com>
2019-01-16 13:37:05 -0500

    Add files via upload

---
+++ interface/src/DefaultFormatter.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+public class DefaultFormatter implements NumberFormatter
+{
+	Scanner in = new Scanner(System.in);
+	public String format(int n)
+	{
+		System.out.println("Enter a number you want formatted");
+		String input = in.nextLine();
+		int stringLength= input.length();
+		int modCommas= stringLength%3;
+		int intInput=Integer.parseInt(input);
+		int commaNumbers= intInput/3;
+		System.out.print(commaNumbers);
+		int newLength=stringLength+commaNumbers;
+		char[] charArray= new char[newLength];
+		if (input.length()>=4)
+		{
+			//for numbers like 100,000 or 100,000,000
+			if (modCommas==0)
+			{
+				for (int i=0; i<input.length(); i+=3)
+				{
+					input.getChars(i, i+2, charArray, 0);
+					charArray[i+3] = ',';
+				}
+				String newString = new String(charArray);
+				return  newString;
+			}
+			//for numbers like 1,000 or 1,000,000
+			else if (modCommas==1)
+			{
+				for (int i=1; i<input.length(); i+=3)
+				{
+					input.getChars(i, i+2, charArray, 0);
+					charArray[i+3] = ',';
+				}
+				String newString = new String(charArray);
+				return  newString;
+			}
+			//for numbers liike 10,000 or 10,000,000
+			else if (modCommas==2)
+			{
+				for (int i=2; i<input.length(); i+=3)
+				{
+					input.getChars(i, i+2, charArray, 0);
+					charArray[i+3] = ',';
+				}
+				String newString = new String(charArray);
+				return  newString;
+			}
+		}
+		else
+		{
+			return input;
+		}
+	}
+}
